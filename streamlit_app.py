ec79237dd185ea217b09a8af33b1a0f559cf9b4c
Mario3113 <mario3113@gmail.com>
2023-10-01 19:42:47 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it as a table.
 streamlit.dataframe(fruityvice_normalized)
 
-streamlight.stop()
+streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
