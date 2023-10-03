a6c0a40b143315baa6e66647a915bff3baf01fef
Mario3113 <mario3113@gmail.com>
2023-10-03 12:24:44 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,5 +66,4 @@ if streamlit.button('Add a Fruit to the list'):
           back_from_function = insert_row_snowflake(add_my_fruit)
           streamlit.text(back_from_function)
           
-streamlit.stop()
 
